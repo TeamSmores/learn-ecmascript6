55f08ec3285fb9f2cc0a1eaeb03c7024fca216b3
Isaac Durand <isaacdurand@Isaacs-MacBook-Air.local>
2016-02-19 12:49:36 -0800

    removed old comments

--- components/ES5Editor.js
+++ components/ES5Editor.js
@@ -6,7 +6,8 @@ import ToolTips from './ToolTips';
 export default class ES5Editor extends Component {
   // I'm basing this code on https://facebook.github.io/react/docs/reusable-components.html
   constructor(props) {
-  	super(props); // What does this do?
+  	super(props); // I'm not sure exactly what this does or even if this is necessary. It looks like this.props is undefined unless passed to super: http://stackoverflow.com/questions/30571875/whats-the-difference-between-super-and-superprops-in-react-when-using-e
+  	// This article may be useful: http://www.jackcallister.com/2015/08/30/the-react-quick-start-guide-es6-edition.html
   	this.state = {es5code: props.initialEs5code};
   }
 
@@ -29,29 +30,14 @@ export default class ES5Editor extends Component {
   }
 
   handleChange(event) {
-  	// console.log('this: ', this); // I console logged to confirm that 'this' refers to ES5Editor
   	this.setState({es5code: event.target.value});
-  	// Console-log event if I can't get this to work.
-  	// When I had just this.setState(state), I saw this error even when I bound: "state is not defined"
-  	// console.log('Handling change! this.state is ', this.state); // I'm learning that this.state is just the default props.
-  	// console.log('Handling change! this.setState is ', this.setState); // It looks like this.setState is correct...
-  	// Why isn't my state changing?
   }
 }
 
 ES5Editor.defaultProps = {initialEs5code: 'Enter your ES5 code here'};
 
-// If this doesn't work, try moving handleChange into the constructor
-
-
-// {this.state.ES5input}
-// Enter your code here
 // Try using a submit input and then a submit button to grab the text in the textarea. We may need to save that text on change.
 
 /* Useful links:
 https://facebook.github.io/react/docs/forms.html
 */
-
-// Notes
-// error message I got from , getInitialState(): 'Cannot find module "./ES5Editor"'
-// got same error message when I tried to rewrite render that way
